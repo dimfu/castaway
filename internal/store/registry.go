4ba0ed9242d1d92344a3d7127505fab89a06063a
dimfu <dimmfu@gmail.com>
2025-08-09 18:44:23 +0800

    fix lost signals by using buffered channels

--- internal/store/registry.go
+++ internal/store/registry.go
@@ -1,10 +1,5 @@
 package store
 
-import "log"
-
-// only allow 32 MB of ram allocation per registry
-const BUF_SIZE = 32 * 1024 * 1024
-
 type FileInfo struct {
 	Name string
 	Size int
@@ -12,59 +7,68 @@ type FileInfo struct {
 }
 
 type Registry struct {
-	Key            string
-	Secret         string
-	FileInfo       *FileInfo
-	Buffer         []byte
-	FreeBufferChan chan struct{}
-	ChunkReadyChan chan struct{}
-	DownloadReady  bool
+	Key      string
+	FileInfo *FileInfo
+
+	// to temporarily store file buffer/blob chunk from the client
+	Buffer []byte
+
+	// channel as a signals so that the transmission is synchronized correctly
+	freeBufferChan chan struct{}
+	chunkReadyChan chan struct{}
+
+	// to indicate that the download is ready and make my life easier on the front end
+	Ready bool
 }
 
-func newRegistry(key string, secret string, fileInfo *FileInfo) *Registry {
-	return &Registry{
+func newRegistry(bufsize int64, key string, fileInfo *FileInfo) *Registry {
+	r := &Registry{
 		Key:            key,
-		Secret:         secret,
 		FileInfo:       fileInfo,
-		Buffer:         make([]byte, 0, BUF_SIZE),
-		FreeBufferChan: make(chan struct{}),
-		ChunkReadyChan: make(chan struct{}),
-		DownloadReady:  false,
+		Buffer:         make([]byte, 0, bufsize),
+		freeBufferChan: make(chan struct{}, 1),
+		chunkReadyChan: make(chan struct{}, 1),
+		Ready:          false,
 	}
+	// initially the buffer is free for writing
+	r.freeBufferChan <- struct{}{}
+	return r
 }
 
 // clear current chunk from the buffer and returns it so that we can send it to the client
 func (r *Registry) DequeueChunk() []byte {
-	for len(r.Buffer) == 0 {
-		<-r.FreeBufferChan // wait until WriteChunks() signals
-	}
+	<-r.chunkReadyChan
 	tmp := make([]byte, len(r.Buffer))
 	copy(tmp, r.Buffer)
 	r.Buffer = r.Buffer[:0]
+	// signal to writer that the r.buffer is ready
+	r.freeBufferChan <- struct{}{}
 	return tmp
 }
 
+// write chunks to registry buffer, and wait for the client to download the buffered chunk
+// so that the chunk buffer can be cleared for the next chunk
 func (r *Registry) WriteChunks(chunk []byte) {
-	if len(r.Buffer) == BUF_SIZE {
-		// wait for free buf channel
-		log.Println("Buffer chunk full, waiting for release")
-		<-r.FreeBufferChan
+	<-r.freeBufferChan // wait for "buffer free"
+	if len(chunk) > cap(r.Buffer) {
+		// clamp to buffer size to avoid overflowing the buffer
+		chunk = chunk[:cap(r.Buffer)]
 	}
-	r.Buffer = append(r.Buffer, chunk...)
-	// signal that chunk is ready
-	r.FreeBufferChan <- struct{}{}
+	r.Buffer = r.Buffer[:len(chunk)]
+	copy(r.Buffer, chunk)
+	r.chunkReadyChan <- struct{}{} // signal "data ready"
 }
 
 func (r *Registry) BuildChunks() []int {
 	// divide the file size with the allowed buf to get the equally same size chunks
-	chunkSize := r.FileInfo.Size / BUF_SIZE
+	chunkSize := r.FileInfo.Size / cap(r.Buffer)
 	// and get the remainder since there is always some case where the distribution is not equal
-	remainder := r.FileInfo.Size % BUF_SIZE
+	remainder := r.FileInfo.Size % cap(r.Buffer)
 
 	chunks := []int{}
 
 	for i := 0; i < chunkSize; i++ {
-		chunks = append(chunks, BUF_SIZE)
+		chunks = append(chunks, cap(r.Buffer))
 	}
 
 	if remainder > 0 {
@@ -72,7 +76,7 @@ func (r *Registry) BuildChunks() []int {
 	}
 
 	if len(chunks) > 0 {
-		r.DownloadReady = true
+		r.Ready = true
 	}
 
 	return chunks
