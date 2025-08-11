914791a08098b496a1dd714e57508f55af8ec4c3
dimfu <dimmfu@gmail.com>
2025-08-11 18:17:31 +0800

    ui improvements with little dots animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
 			colors: {
 				"antique-white": 'var(--antique-white)',
 			},
+			keyframes: {
+				ellipsis: {
+					'0%': { content: '""' },
+					'33%': { content: '"."' },
+					'66%': { content: '".."' },
+					'100%': { content: '"..."' },
+				}
+			},
+			animation: {
+				ellipsis: 'ellipsis 1s steps(3, end) infinite'
+			}
 		},
 	}
 };
